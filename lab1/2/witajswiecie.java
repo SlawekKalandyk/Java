b6fde21fa709a8a8cafd78bdc3cd463286244715
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2018-10-08 20:34:48 +0200

    up

---
+++ lab1/2/witajswiecie.java
@@ -0,0 +1,13 @@
+/**
+ * Klasa wyswietlajaca powitaniena konsoli.
+ * @author Szymon
+ */
+public class HelloWorld {
+    /**
+     *
+     * @param argv lista argumentow przekazywana do programu
+     */
+   public static void main(String [] argv){
+       System.out.println("Hello World!");
+  }
+}
