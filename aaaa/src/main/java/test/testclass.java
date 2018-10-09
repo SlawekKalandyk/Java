68c99e290f8c86bc73cb61159c9d69c6e70e8f9a
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2018-10-09 12:08:05 +0200

    up

--- aaaa/src/main/java/test/testclass.java
+++ aaaa/src/main/java/test/testclass.java
@@ -2,6 +2,7 @@ package test;
 
 public class testclass {
     public static void main(String[] argv) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
+        branchtestclass.helloagain();
     }
 }
