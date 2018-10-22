bb3fc7887088491bfa8dcffbb453451058d33462
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2018-10-22 19:31:29 +0200

    java email sending works

--- lab3/emailMessenger/src/main/java/mainpack/EmailMessage.java
+++ lab3/emailMessenger/src/main/java/mainpack/EmailMessage.java
@@ -26,9 +26,21 @@ public class EmailMessage {
     }
 
     public void send() {
-        Properties properties = System.getProperties();
-        properties.setProperty("mail.smtp.host", "localhost");
-        Session session = Session.getDefaultInstance(properties);
+        final String username = "";
+        final String password = "";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
 
         try {
             MimeMessage message = new MimeMessage(session);
--- lab3/emailMessenger/src/main/java/mainpack/MainClass.java
+++ lab3/emailMessenger/src/main/java/mainpack/MainClass.java
@@ -6,8 +6,8 @@ import java.util.*;
 public class MainClass {
     public static void main(String[] args) {
         EmailMessage msg = EmailMessage.builder()
-                .addFrom("slawomir.kalandyk@gmail.com")
-                .addTo("slawomir.kalandyk@gmail.com")
+                .addFrom("")
+                .addTo("")
                 .addSubject("Mail testowy")
                 .addContent("Brak tresci")
                 .build();
